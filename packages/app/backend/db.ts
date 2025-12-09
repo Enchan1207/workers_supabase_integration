61939429d3a0289e6fea09a61e04dde1f69bcf22
enchan <enchant0130@gmail.com>
2025-12-09 22:26:22 +0900

    chore: #1 use postgres instead of pg

--- packages/app/backend/db.ts
+++ packages/app/backend/db.ts
@@ -2,6 +2,7 @@ import type { PostgresJsDatabase } from 'drizzle-orm/postgres-js'
 import { drizzle } from 'drizzle-orm/postgres-js'
 import { env } from 'hono/adapter'
 import { createMiddleware } from 'hono/factory'
+import postgres from 'postgres'
 
 type Variables = {
   drizzle: PostgresJsDatabase
@@ -11,7 +12,8 @@ export const dbMiddleware = createMiddleware<{
   Bindings: Env
   Variables: Variables
 }>((c, next) => {
-  const db = drizzle(env(c).DATABASE_URL)
+  const client = postgres(env(c).DATABASE_URL, { prepare: false })
+  const db = drizzle({ client })
   c.set('drizzle', db)
 
   return next()
