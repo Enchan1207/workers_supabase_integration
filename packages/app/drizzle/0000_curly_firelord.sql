4f7c2babc5628cf0e8a3014f7cc69e920a43ffd8
enchan <enchant0130@gmail.com>
2025-12-09 22:53:29 +0900

    chore: add migration file

---
+++ packages/app/drizzle/0000_curly_firelord.sql
@@ -0,0 +1,7 @@
+CREATE TABLE "posts" (
+	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "posts_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
+	"title" text NOT NULL,
+	"content" text NOT NULL
+);
+--> statement-breakpoint
+ALTER TABLE "posts" ENABLE ROW LEVEL SECURITY;
