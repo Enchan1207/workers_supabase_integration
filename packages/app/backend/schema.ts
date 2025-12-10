634ff77185867f70170f821ddb7f7b95277f4d66
enchan <enchant0130@gmail.com>
2025-12-10 19:35:12 +0900

    feat: #3 テーブルにuser_idを追加し、複合種キーに変更

--- packages/app/backend/schema.ts
+++ packages/app/backend/schema.ts
@@ -1,7 +1,12 @@
-import { integer, pgTable, text } from 'drizzle-orm/pg-core'
+import { integer, pgTable, primaryKey, text } from 'drizzle-orm/pg-core'
 
-export const postsTable = pgTable('posts', {
-  id: integer().primaryKey().generatedAlwaysAsIdentity(),
-  title: text().notNull(),
-  content: text().notNull(),
-}).enableRLS()
+export const postsTable = pgTable(
+  'posts',
+  {
+    id: integer(),
+    user_id: text(),
+    title: text().notNull(),
+    content: text().notNull(),
+  },
+  (table) => [primaryKey({ columns: [table.id, table.user_id] })],
+).enableRLS()
