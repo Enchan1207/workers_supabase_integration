9a1eb3c1f4578a1b552251f77b485bd75434a75f
enchan <enchant0130@gmail.com>
2025-12-12 09:28:18 +0900

    chore: rename script

--- infra/modules/env_merge/main.tf
+++ infra/modules/env_merge/main.tf
@@ -7,7 +7,7 @@ variable "auth0_app_client_id" {
 }
 
 data "external" "merge_environment" {
-  program = ["pnpm", "-F", "infra-scripts", "exec", "tsx", "src/setenv.ts", "--mode", var.mode]
+  program = ["pnpm", "-F", "infra-scripts", "exec", "tsx", "src/update-env.ts", "--mode", var.mode]
   query = {
     VITE_AUTH0_CLIENT_ID = var.auth0_app_client_id
   }
